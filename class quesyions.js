84649bc06194b5784cca7fa11e5286c74a955c11
Deepanshu <dgulia424@gmail.com>
2026-06-21 21:48:54 +0530

    Add files via upload

---
+++ class quesyions.js
@@ -0,0 +1,79 @@
+// Question=1
+
+let Num=prompt("enter a Num :");
+
+if(Num%2==0){
+    console.log("even no.");
+}else{
+    console.log("odd no.");
+}
+
+// question=2
+
+let Num1=prompt("enter a Num :");
+let Num2=prompt("enter a Num :");
+
+if(Num1>Num2){
+    console.log("Numi is larger the Num2 :",Num1);
+}else{
+    console.log("Numi is larger the Num1 :",Num2);
+}
+
+// question=3
+let Num1=prompt("enter a Num :");
+let Num2=prompt("enter a Num :");
+let Num3=prompt("enter a Num :");
+
+if(Num1>Num2){
+    console.log("Numi is larger the Num1 :",Num1);
+    if(Num2>Num3){
+    console.log("Numi is larger the Num2 :",Num2);
+    if(Num3>Num1){
+        console.log("Numi is larger the Num3 :",Num3);
+}
+    }
+}
+else {
+}
+
+// question=4
+
+let Num1=prompt("enter a Num :");
+
+if(Num1%3==0  && Num1%5==0){
+    console.log("True");
+}else{
+    console.log("False");
+}
+
+// question=5
+
+let Num1=prompt("enter a Num :");
+let Num2=prompt("enter a Num :");
+let ope=prompt("enter a ope :");
+let result=0;
+ if(ope='+'){
+    console.log(Num1+Num2);
+   
+
+ if(ope='-'){
+    console.log(Num1+Num2);
+     
+ if(ope='*'){
+    console.log(Num1+Num2);
+ 
+ if(ope='/'){
+    console.log(Num1+Num2);
+ }
+}
+ }
+ }
+else{
+
+}
+
+
+
+
+
+
